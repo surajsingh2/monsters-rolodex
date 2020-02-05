3e55128c2064499a0769728e015997f3df36d6fc
Suraj Singh <mesuraj30@gmail.com>
2020-02-05 22:50:32 +0530

    Updates

---
+++ precache-manifest.5a51c30b74201bd61ce406bc2ac4b611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368ae9182314a4d00a1220f4a1b89975",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0911a325289c257f66bf",
+    "url": "/monsters-rolodex/static/css/main.1cb04b05.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0911a325289c257f66bf",
+    "url": "/monsters-rolodex/static/js/main.bf8ab0dd.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
